4fd7fa107e0842418fa3430e7a14e576d396ab9c
Frederic Branca <frederic.branca@gmail.com>
2023-03-27 15:20:41 +0200

    header('Location') + add security filter

---
+++ Traitement.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+if (isset($_POST['submit'])) {
+    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS); // empèche injection de SQL ou de HTML, supprime toutes les balises
+
+    $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION); //FILTER_VALIDATE_FLOAT (champ "price") : validera le prix que s'il est un nombre à virgule (pas de texte ou autre…), le drapeau FILTER_FLAG_ALLOW_FRACTION est ajouté pour permettre l'utilisation du caractère "," ou "." pour la décimale.
+    
+    $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT); //ne validera la quantité que si celle-ci est un nombre entier différent de zéro (qui est considéré comme nul)
+}
+
+header("Location:index.php");
