efa39ff3ee6a2ff20db29deb8799d3704487993b
Frederic Branca <frederic.branca@gmail.com>
2023-03-28 08:46:51 +0200

    Enregistrement du produit en session

--- Traitement.php
+++ Traitement.php
@@ -8,8 +8,14 @@ if (isset($_POST['submit'])) {
     $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION); //FILTER_VALIDATE_FLOAT (champ "price") : validera le prix que s'il est un nombre à virgule (pas de texte ou autre…), le drapeau FILTER_FLAG_ALLOW_FRACTION est ajouté pour permettre l'utilisation du caractère "," ou "." pour la décimale.
     
     $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT); //ne validera la quantité que si celle-ci est un nombre entier différent de zéro (qui est considéré comme nul)
-}
 
+    if($name && $price && $qtt) {
+
+        $product = ["name" => $name, "price" => $price, "qtt" => $qtt, "total" => $price*$qtt];
+    
+        $_SESSION['products'][] = $product;
+    }
+}
 
 header("Location:index.php");
 
